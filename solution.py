d9d43918d0b95c7a80ed16a2033157de0129d255
nabeelsherazi <nabeelsherazi97@gmail.com>
2017-11-04 17:20:08 -0400

    inital commit

---
+++ solution.py
@@ -0,0 +1,11 @@
+# Scaffolding
+
+# Opening and handling CSV files code will go here
+# Will have list of projects as tuples, in structure:
+# (name, location, inital_cost, co2_reduction, total_payback)
+# Types: (string, string, int, int, int)
+# Provided as projects = [p1, p2, etc...]
+
+
+
+# Knapsack problem solution goes under here.
